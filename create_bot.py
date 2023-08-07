bf8c1c58c2c35a4148d429e8bac87dc407e567c8
Sovwva <97623978+Sovwva@users.noreply.github.com>
2023-08-07 16:06:35 +0200

    init project

--- create_bot.py
+++ create_bot.py
@@ -2,7 +2,14 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+import json
+
 storage = MemoryStorage()
 
-bot = Bot(token='')
+with open("config.json", "r") as json_file:
+    json_data = json.load(json_file)
+
+token = json_data["bot_token"]
+
+bot = Bot(token)
 dp = Dispatcher(bot, storage=storage)
